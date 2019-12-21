0704ff4fa9e2fb0af13a8b9b04c78fb92f96d629
Aditya Telange <21258296+AdityaTelange@users.noreply.github.com>
2019-12-21 13:38:12 +0530

    output .

--- entry_point.sh
+++ entry_point.sh
@@ -2,10 +2,9 @@ echo "##### installing requrements"
 # adding symbolic link | fixes: ERROR: /bin/sh does not point to bash
 ln -sf bash /bin/sh > /dev/null 2>&1
 # Binutils, gcc , g++, xz, make, patch
-apt-get -y update > /dev/null 2>&1
-apt-get install -y --no-install-recommends build-essential > /dev/null 2>&1
-# bison gwak m4 python3 makeinfo wget ca-certificates
-apt-get install -y --no-install-recommends ncurses perl wget ca-certificates > /dev/null 2>&1
+apt-get -y update 
+apt-get install -y --no-install-recommends build-essential
+apt-get install -y ncurses perl wget ca-certificates
 
 
 
